9ce1cb08e567f9500285bd2bceff4263d4d3c7df
Isabella de Leon <ijdeleon@berkeley.edu>
2018-12-18 22:48:21 -0800

    working marker click, removes marker on click

--- app/src/main/java/com/foodmap/foodmap/MainActivity.java
+++ app/src/main/java/com/foodmap/foodmap/MainActivity.java
@@ -7,6 +7,10 @@ import com.mapfit.android.MapView;
 import com.mapfit.android.Mapfit;
 import com.mapfit.android.MapfitMap;
 import com.mapfit.android.OnMapReadyCallback;
+import com.mapfit.android.annotations.Marker;
+import com.mapfit.android.annotations.MarkerOptions;
+import com.mapfit.android.annotations.callback.OnMarkerClickListener;
+import com.mapfit.android.geometry.LatLng;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -17,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
 
         // if you have an Application class, you should call this from it
         Mapfit.getInstance(this, "591dccc4e499ca0001a4c6a4ff0a7de04c0346d3bc0b3a0b682f0476");
@@ -31,8 +33,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMapReady(@NotNull MapfitMap mapfitMap) {
                 // mapfitMap is ready to be used!
+                LatLng latLng = new LatLng(40.744023, -73.993150);
+                mapfitMap.addMarker(new MarkerOptions().position(latLng));
+
+
+                mapfitMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+                    @Override
+                    public void onMarkerClicked(Marker m) {
+                        // Do whatever you want in here
+                        m.remove();
+                    }
+                });
             }
+
+
+
         });
+
+
+
+
+
     }
 
     @Override
@@ -46,4 +67,6 @@ public class MainActivity extends AppCompatActivity {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
+
 }
