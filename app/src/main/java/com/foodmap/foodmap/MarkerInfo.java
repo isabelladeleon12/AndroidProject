b3faab6f1f0f11a57aea929400780c32b92b2282
Isabella de Leon <ijdeleon@berkeley.edu>
2018-12-18 22:47:47 -0800

    working event listener for clicking on marker

---
+++ app/src/main/java/com/foodmap/foodmap/MarkerInfo.java
@@ -0,0 +1,13 @@
+package com.foodmap.foodmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarkerInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker_info);
+    }
+}
